52975a440ceca45fbdd9df1e328840f944caffd2
Gianmembreno <66712565+Gianmembreno@users.noreply.github.com>
2022-08-23 11:24:31 -0400

    Hello, Cargo!

---
+++ hello_cargo/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+
+/*
+cargo new [create project]
+cargo build [build project]
+cargo run [build and run project in one step]
+cargo check [build without binary]
+*/
+
